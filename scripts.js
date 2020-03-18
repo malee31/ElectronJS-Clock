2b572691238c0c90c6ec1ade801cc0613fad51cd
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-03-17 19:11:14 -0700

    Seconds working, js not.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-alert("Scripted");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const now = new Date();
+
+var hr = now.getHours();
+var min = now.getMinutes();
+var sec = now.getSeconds();
+
+
+hour.setAttribute("animation-delay", hr * -60);
+minute.setAttribute("animation-delay", min * -60);
+second.setAttribute("animation-delay", sec * -60);
